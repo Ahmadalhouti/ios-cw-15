2fcc701e66f9dd9e97f9affb640b7d9656e8cc20
Ahmad Alhouti <ahmadalh3500@gmail.com>
2022-03-19 11:07:35 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+struct student {
+    let name: String
+    let age: Int
+    let garde: Double
+    let attendence: Bool
+}
+var ahmad = student(name:"ahmad", age:17, garde:12 , attendence:true )
+
+print(ahmad)
